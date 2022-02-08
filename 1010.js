95a50e423a813f2e51c6738720807d57b81c45d7
rudolf0328 <ck0716080@gmail.com>
2022-02-08 23:32:51 +0900

    1010

--- 1010.js
+++ 1010.js
@@ -1,5 +1,5 @@
 let fs = require('fs');
-let input = fs.readFileSync('input.txt').toString().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 let count = input[0];
 let numbers = [];
